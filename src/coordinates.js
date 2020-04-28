5732fa4ea638b66bc26e75ff76db887cb80ce1b4
Johan Djärv Karltorp <johan.djarv.karltorp@gmail.com>
2020-04-28 08:44:28 +0200

    generate coordinates inside specified country

---
+++ src/coordinates.js
@@ -0,0 +1,118 @@
+const process = require("process");
+const bt = require('geojson-bounds');
+const classifyPoint = require("robust-point-in-polygon");
+const countries = require('./countries-geojson.json');
+const LoadingBar = require('./loadingBar');
+
+const loadingBarSize = 50;
+const loadingBar = new LoadingBar(loadingBarSize);
+
+const country = {
+    get: (id) => countries.features.find((element) => element.properties.ISO_A3 === id),
+    bounds: (shape) => bt.envelope(shape.geometry),
+    coordinates: (shape, nrOfCoordinates) => {
+        let data = {
+            type: "FeatureCollection",
+            features: []
+        };
+
+        let coordinates;
+        let countryName = shape.properties.ADMIN;
+        let amount = nrOfCoordinates.toLocaleString();
+        const start = process.hrtime();
+
+        console.log(`Creating \x1b[1m\x1b[32m${amount}\x1b[0m random coordinates` +
+            ` within ${countryName}'s borders`);
+
+        loadingBar.init();
+
+        for (let i = 0; i < nrOfCoordinates; i++) {
+            coordinates = getCoordinates(shape);
+            data.features.push(toGeoJSON(coordinates));
+            loadingBar.update(i, nrOfCoordinates);
+        }
+
+        loadingBar.update(nrOfCoordinates, nrOfCoordinates);
+        printCompletionTime(start);
+
+        return data;
+    }
+};
+
+
+function toGeoJSON(coordinates) {
+    let lat = 0;
+    let lng = 1;
+
+    return {
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: coordinates
+        },
+        properties: {
+            popup: `<b>Lat:&nbsp;&nbsp;${coordinates[lat]}<br> Lng: ${coordinates[lng]}</b><br>` +
+                "<span onclick='copyCoords(this)' onmouseout='reset(this)" +
+                "'class='mdi mdi-content-copy tooltip'>" +
+                `<span class='tooltiptext'>Copy</span></span><p>[${coordinates}]</p>`,
+        }
+    };
+}
+
+
+function getPolygon(shape, index) {
+    let first = 0;
+
+    if (shape.geometry.type === 'MultiPolygon') {
+        return shape.geometry.coordinates[index][first];
+    } else if (shape.geometry.type === 'Polygon') {
+        return shape.geometry.coordinates[index];
+    }
+}
+
+function getCoordinates(shape) {
+    let bound = bt.extent(shape.geometry);
+    let west = 0, south = 1, east = 2, north = 3;
+    let point = [getRandom(bound[west], bound[east]), getRandom(bound[south], bound[north])];
+    let match = false;
+    let insidePolygon = -1;
+
+    while (!match) {
+        for (let i = 0; i < shape.geometry.coordinates.length; i++) {
+            let polygon = getPolygon(shape, i);
+
+            if (classifyPoint(polygon, point) === insidePolygon) {
+                match = true;
+                break;
+            }
+        }
+        if (!match) {
+            point = [getRandom(bound[west], bound[east]), getRandom(bound[south], bound[north])];
+        }
+    }
+
+    return point;
+}
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function printCompletionTime(start) {
+    let minutes, seconds, milliseconds, nanoseconds;
+    let oneMillion = 1000000;
+    let oneMinutes = 60;
+    let zero = 0;
+
+    [seconds, nanoseconds] = process.hrtime(start);
+
+    minutes = Math.floor(seconds / oneMinutes);
+    seconds = seconds - (minutes * oneMinutes);
+    milliseconds = Math.floor(nanoseconds / oneMillion);
+
+    let minutesString = minutes > zero ? `${minutes} minutes and ` : "";
+
+    console.info('Completed in %s%d.%d seconds\n', minutesString, seconds, milliseconds);
+}
+
+module.exports = country;
---
+++ src/loadingBar.js
@@ -0,0 +1,40 @@
+const rdl = require("readline");
+
+class LoadingBar {
+    constructor(size) {
+        this.size = size;
+        this.cursor = 0;
+    }
+
+    init() {
+        process.stdout.write("\n");
+        process.stdout.write("\x1B[?25l");
+        process.stdout.write("[");
+        for (let i = 0; i < this.size; i++) {
+            process.stdout.write("\u2022");
+        }
+        process.stdout.write("]");
+        this.cursor = 1;
+        rdl.cursorTo(process.stdout, this.cursor);
+    }
+
+    update(processed, total) {
+        // eslint-disable-next-line no-magic-numbers
+        let onePercentOfSize = this.size / 100;
+        let toInt = 100;
+        let percent = (processed / total) * toInt;
+        let percentOfSize = percent * onePercentOfSize;
+
+        for (let i = this.cursor; i <= percentOfSize; i++) {
+            process.stdout.write("\u2588");
+            this.cursor++;
+        }
+
+        if (processed === total) {
+            process.stdout.write("] ");
+            process.stdout.write("\x1B[?25h");
+        }
+    }
+}
+
+module.exports = LoadingBar;
